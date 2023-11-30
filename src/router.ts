1cd0e98278b6ab1a295d621651cdb3d8af57ebb2
ankitesh <ankitesh4@gmail.com>
2023-11-30 23:07:28 +0530

    middle ware added

--- src/router.ts
+++ src/router.ts
@@ -3,22 +3,26 @@ import { Router } from 'express';
 const router = Router();
 
 // Products
-router.get('/product', (req, res) => {});
-router.get('/product/:id', (req, res) => {});
-router.put('/product/:id', (req, res) => {});
-router.post('/product', (req, res) => {});
-router.delete('/product/:id', (req, res) => {});
+router.get('/product', (req, res) => {
+  res.json({ message: 'hello' });
+});
+router.get('/product/:id', () => {});
+router.put('/product/:id', () => {});
+router.post('/product', () => {});
+router.delete('/product/:id', () => {});
 
 // Update
-router.get('/update', (req, res) => {});
-router.get('/update/:id', (req, res) => {});
-router.put('/update/:id', (req, res) => {});
-router.post('/update', (req, res) => {});
-router.delete('/update/:id', (req, res) => {});
+router.get('/update', () => {});
+router.get('/update/:id', () => {});
+router.put('/update/:id', () => {});
+router.post('/update', () => {});
+router.delete('/update/:id', () => {});
 
 // Update Points
-router.get('/updatepoint', (req, res) => {});
-router.get('/updatepoint/:id', (req, res) => {});
-router.put('/updatepoint/:id', (req, res) => {});
-router.post('/updatepoint', (req, res) => {});
-router.delete('/updatepoint/:id', (req, res) => {});
+router.get('/updatepoint', () => {});
+router.get('/updatepoint/:id', () => {});
+router.put('/updatepoint/:id', () => {});
+router.post('/updatepoint', () => {});
+router.delete('/updatepoint/:id', () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// custom  middleware
+// app.use((req, res, next) => {
+//   req.shhhhhh = 'hello';
+//   next();
+// });
 
 app.get('/', (req, res) => {
   console.log('hello from server');
